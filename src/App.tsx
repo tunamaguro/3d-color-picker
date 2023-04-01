708e44c1b45300b02f6d63769ef78ce7bd977d5e
tunamaguro <tsu7magu6@gmail.com>
2023-04-01 14:50:17 +0000

    球コンポーネント

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { Canvas } from "@react-three/fiber";
 import { Box } from "./components/Box";
+import { Sphere } from "./components/Sphere";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
 	return (
 		<Canvas style={{ width: window.innerWidth, height: window.innerHeight }}>
 			<ambientLight />
+			<OrbitControls />
 			<pointLight position={[10, 10, 10]} />
-			<Box position={[-1.2, 0, 0]} />
-			<Box position={[1.2, 0, 0]} />
+			<Sphere position={[0, 0, 0]} />
 		</Canvas>
 	);
 }
---
+++ src/components/Sphere/index.tsx
@@ -0,0 +1,16 @@
+import { ThreeElements, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+import { Mesh } from "three";
+
+type Props = ThreeElements["mesh"];
+
+export function Sphere({ ...props }: Props) {
+	const mesh = useRef<Mesh>(null!);
+
+	return (
+		<mesh {...props} ref={mesh}>
+			<sphereGeometry args={[3, 12, 12]} />
+			<meshStandardMaterial wireframe={true} color={"orange"} />
+		</mesh>
+	);
+}
