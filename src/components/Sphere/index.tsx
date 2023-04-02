0dc7d3aa21123f129e3ef2ac74c34267c59611b9
tunamaguro <tsu7magu6@gmail.com>
2023-04-02 14:38:06 +0000

    それっぽい色相環になった

--- src/components/Sphere/index.tsx
+++ src/components/Sphere/index.tsx
@@ -2,15 +2,20 @@ import { ThreeElements } from "@react-three/fiber";
 import { useRef } from "react";
 import { Mesh } from "three";
 
-type Props = ThreeElements["mesh"];
+type Props = ThreeElements["mesh"] & {
+	color?: number | string;
+	position: [number, number, number];
+};
 
-export function Sphere({ ...props }: Props) {
+export function Sphere({ color, position, ...props }: Props) {
 	const mesh = useRef<Mesh>(null!);
 
+	const size = position[2] === 0 ? 0.3 : 0.1;
+
 	return (
-		<mesh {...props} ref={mesh}>
-			<sphereGeometry args={[0.3, 12, 12]} />
-			<meshStandardMaterial wireframe={true} color={"orange"} />
+		<mesh position={position} {...props} ref={mesh}>
+			<sphereGeometry args={[size, 12, 12]} />
+			<meshStandardMaterial color={color ?? "orange"} />
 		</mesh>
 	);
 }
