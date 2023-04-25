5979e4642f930f561a2036606fe83a7be75eff3f
tunamaguro <tsu7magu6@gmail.com>
2023-04-25 12:16:24 +0000

    円筒配置に変更変更

--- src/components/HslCylinder/index.tsx
+++ src/components/HslCylinder/index.tsx
@@ -1,40 +1,28 @@
 import range from "just-range";
-import { XyzCoord } from "../../coord";
+import { HslCoord } from "../../coord";
 import { Sphere } from "../Sphere";
 
 const baseR = 10;
-const rads = range(0, 2 * Math.PI, Math.PI / 20);
-const circle = rads.map((rad) => {
-	const x = Math.cos(rad) * baseR;
-	const y = Math.sin(rad) * baseR;
-	const z = 0;
-	const pos = new XyzCoord(x, y, z).turn_Xaxis(90);
-	return pos;
-});
-
-const onionRads = [0, 45, 60, 75];
-const onion = onionRads.flatMap((theta) =>
-	circle.map((xyz) => {
-		const [_x, y, _z] = xyz.to_vec();
-		const [u, _v, w] = xyz.turn_Zaxis(theta).to_vec();
-		return new XyzCoord(u, y, w);
-	}),
-);
-
-const turn = range(0, 360, 15);
-const sphere = turn.flatMap((theta) =>
-	onion.map((xyz) => xyz.turn_Zaxis(theta)),
+const hue = range(0, 360, 20);
+const saturation = range(10, 101, 15);
+const lightness = range(0, 101, 10);
+const concentricCircles = lightness.flatMap((l, light_idx) =>
+	hue.flatMap((h) =>
+		saturation.map((s) => new HslCoord((h + (light_idx % 2) * 10) % 360, s, l)),
+	),
 );
 
 export function HslCylinder() {
 	return (
 		<>
-			{sphere.map((pos) => (
-				<Sphere
-					position={pos.to_vec()}
-					color={pos.to_hsl().to_rgb().to_hex()}
-				/>
-			))}
+			{concentricCircles.map((hsl) => {
+				return (
+					<Sphere
+						position={hsl.to_xyz(baseR).to_vec()}
+						color={hsl.to_rgb().to_hex()}
+					/>
+				);
+			})}
 		</>
 	);
 }
